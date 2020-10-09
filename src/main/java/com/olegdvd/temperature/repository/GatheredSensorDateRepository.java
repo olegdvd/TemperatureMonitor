37c407257fdccba308dd52a1fcc6d04be12fb440
Oleg <oleg.dvd@gmail.com>
2020-10-10 00:41:01 +0300

    Added cross origin to all hosts ("*") (for development purpose) Added method to gather Sensor data from repo filtered by time

--- src/main/java/com/olegdvd/temperature/repository/GatheredSensorDateRepository.java
+++ src/main/java/com/olegdvd/temperature/repository/GatheredSensorDateRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface GatheredSensorDateRepository extends JpaRepository <GatheredSensorData, String> {
 
     List<GatheredSensorData> findAllBySensorId(String sensorId);
+
 }
